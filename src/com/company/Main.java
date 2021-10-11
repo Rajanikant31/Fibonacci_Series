1b54db1d30729616ddb30d8635afcd922b85bc40
Rajanikant <rajanikantpawar31@gmail.com>
2021-10-11 23:15:39 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+class Main {
+    public static void main(String[] args) {
+
+        int n = 10, a = 0 , b = 1;
+        System.out.println("Fibonacci Series till " + n + " terms:");
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(a + ", ");
+
+            // compute the next term
+            int c = a + b ;
+            a = b;
+            b = c;
+        }
+    }
+}
